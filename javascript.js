b63f346f8a80a5de1511bfcb708e95d74b48442a
soyosa <110913089+soyosa@users.noreply.github.com>
2023-06-24 18:34:23 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+console.log ("hello worlds")
+
+/* 1 */ 
+console . log (23 +97)
+
+/* 2 */
+var numbers =0;
+for (var x = 1; x <= 5; x++) 
+    {numbers += x }
+console . log (numbers)
+
+/* 3 */
+var s = 4+6+9;
+var solution = s/77
+console . log (solution.toFixed(5))
+
+/* 4 */
+let a = 10
+
+let b = 7 * a
+console . log (b)
+
+/* 5 */
+const max = 57
+var actual = max - 13
+var percentage =actual / max
+console . log (percentage.toFixed(4))
+
+/* 6 */
+console . log ()
