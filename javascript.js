036309e08f00eec349f2556b29e05566b4c1651f
soyosa <110913089+soyosa@users.noreply.github.com>
2023-06-26 18:50:14 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,30 +1,25 @@
-console.log ("hello worlds")
-
 /* 1 */ 
-console . log (23 +97)
+console.log(23 +97)
 
 /* 2 */
 var numbers =0;
-for (var x = 1; x <= 5; x++) 
-    {numbers += x }
-console . log (numbers)
+for (var i=1; i<= 5; i++) 
+    {numbers += i }
+console.log(numbers)
 
 /* 3 */
-var s = 4+6+9;
-var solution = s/77
-console . log (solution.toFixed(5))
+var operation =4+6+9;
+var solution = operation /77
+console.log(solution.toFixed(5))
 
 /* 4 */
-let a = 10
+let FirstNumber= 10
 
-let b = 7 * a
-console . log (b)
+let result = 7 * a
+console.log(result)
 
 /* 5 */
 const max = 57
 var actual = max - 13
 var percentage =actual / max
-console . log (percentage.toFixed(4))
-
-/* 6 */
-console . log ()
+console.log(percentage.toFixed(4))
